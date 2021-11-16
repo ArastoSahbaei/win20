fc58de35a623343fc18592ee72208d3fc4b341d7
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-16 09:37:33 +0100

    updated

--- src/views/home/HomeView.jsx
+++ src/views/home/HomeView.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react'
+
 export const HomeView = () => {
+   const [value, setValue] = useState(0)
+   const [isLoading, setIsLoading] = useState(true)
+
+   useEffect(() => {
+      alert('First render')
+      setTimeout(() => { setIsLoading(false) }, 3000);
+      return (() => {
+         alert('Last render')
+      })
+   }, [value])
+
+   /*    componentWillMount(){ }
+      componentDidUpdate(){ }
+      componentWillUnMount(){ } */
+
    return (
       <div>
-         <h1>This is the Homeview</h1>
+         {!isLoading && <h1>Is no longer loading</h1>}
       </div>
    )
 }
--- src/views/movies/MoviesView.jsx
+++ src/views/movies/MoviesView.jsx
@@ -1,14 +1,30 @@
 import styled from "styled-components"
+import Teacher from '../../shared/images/teacher.svg'
 
 export const MoviesView = () => {
    return (
-      <div>
-         <H1 isColored="blue">NjetFlix</H1>
+      <Div>
          <H1>NjetFlix</H1>
-      </div>
+         <Image src={Teacher} alt={'Error'} />
+      </Div>
    )
 }
 
+const Div = styled.div`
+   display: grid;
+   grid-template-columns: repeat(12, 1fr);
+   grid-template-rows: repeat(3, 1fr);
+   background-color: orange;
+`
+
 const H1 = styled.h1`
    color: ${props => props.isColored ? props.isColored : 'red'};
+   grid-column: 8/9;
+   grid-row: 2/3;
+`
+
+const Image = styled.img`
+   width: 100%;
+   grid-column: 1/2;
+   grid-row: 1/1;
 `
