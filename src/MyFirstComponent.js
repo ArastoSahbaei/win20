4a6f2da7298295f96a3fc095d414dc1d3082fdd5
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-16 07:58:52 +0100

    updated

--- src/MyFirstComponent.js
+++ src/MyFirstComponent.js
@@ -1,8 +1,11 @@
 export const MyFirstComponent = ({ name, age }) => {
+
+   const randomString = 'randomString'
+   const secondaryString = `This is the randomstring: ${randomString}`
+
    return (
       <div>
          <h1>Whatsup {name} age {age}</h1>
       </div>
    )
 }
- 
---
+++ src/shared/styles/animations/fadeIn.js
@@ -0,0 +1,15 @@
+import { keyframes } from "styled-components";
+
+export const fadeIn = keyframes`
+0% {
+   opacity: 0;
+   color: red;
+}
+50% {
+   color: green;
+}
+100% {
+   opacity: 1;
+   color: hotpink;
+}
+`
